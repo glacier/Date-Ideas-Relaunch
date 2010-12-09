53482ed5c04d1e8bdbcebc0cca7e10f20fe71172
root <root@li248-56.members.linode.com>
2010-12-09 03:07:45 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloWorld::Application.config.secret_token = 'd08a9cd4fef96220434a6e4d8a986b855334c3ab0e664eff59028ce0bcbad66473023b5521bb915ce9585ed3b3150a32af781d13270295c6566234095d15063a'
