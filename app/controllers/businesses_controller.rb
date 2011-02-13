e3e48169d74d3cbe541035754ae8cb38940fa776
Grace Li <fullofgrace88@gmail.com>
2011-02-12 20:58:47 -0500

    added current fixtures for business; and updated migrations

---
+++ app/controllers/businesses_controller.rb
@@ -0,0 +1,51 @@
+class BusinessesController < ApplicationController
+  def index
+    @businesses = Business.all
+  end
+  
+  def show
+    @business = Business.find(params[:id])
+  end
+  
+  def edit
+    @business = Business.find(params[:id])
+  end
+  
+  def new
+    @business = Business.new
+    respond_to do |format|
+       format.html # new.html.erb
+       format.xml  { render :xml => @post }
+     end
+  end
+  
+  def create
+    @business = Business.new(params[:business])
+    
+    respond_to do |wants|
+      if @business.save
+        flash[:notice] = 'Business was successfully created.'
+        wants.html { redirect_to(@business) }
+        wants.xml { render :xml => @business, :status => :created, :location => @business }
+      else
+        wants.html { render :action => "new" }
+        wants.xml { render :xml => @business.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+  
+  
+  def update
+    y params[:business]
+    @business = Business.find(params[:id])  
+    # redirect_to(business_url(@venue.id))
+    if @business.update_attributes(params[:business])
+           redirect_to(businesses_path, :notice => 'Venue listing was successfully updated.')
+    end
+  end
+  
+  def destroy
+    @business = Business.find(params[:id])
+    @business.destroy
+  end
+end
---
+++ db/migrate/20110125021326_create_businesses.rb
@@ -0,0 +1,46 @@
+class CreateBusinesses < ActiveRecord::Migration
+  def self.up
+    # db normalizations needed?
+    # order the Excel columns according to this migration
+    # does the order of columns listed in the migration matter in anyway?
+    create_table :businesses do |t|
+      t.string :venue_type
+      t.string :logo
+      t.string :name
+      t.string :address1
+      # t.string :address2
+      # t.string :address3
+      t.string :city
+      t.string :province
+      t.string :postal_code
+      t.string :country
+      t.float  :latitude
+      t.float  :longitude
+      t.string :photo_url
+      t.string :url
+      t.string :phone_no
+      t.string :website
+      t.string :dna_excerpt
+      # t.float :distance
+      # t.float :avg_rating
+      t.string :dna_neighbourhood
+      t.string :dna_atmosphere
+      t.string :dna_pricepoint # should be decimal?
+      t.string :dna_category
+      # add a column for food allergies?
+      # add a column for accessibility?
+      t.string :dna_dresscode
+      t.string :dna_pictures
+      t.string :dna_review
+      # t.string :post_title # belongs in another table
+      t.integer :dna_rating_conversation # ratings should be in another table?
+      t.integer :dna_rating_convenience
+      t.integer :dna_rating_comfort
+      t.timestamps
+    end 
+  end
+
+  def self.down
+    drop_table :businesses
+  end
+end
