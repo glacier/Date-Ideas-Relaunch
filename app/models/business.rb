932134399af2004cf7dcc253c5989fc07978250e
Date Ideas <dateideas@li248-56.members.linode.com>
2011-02-16 02:19:55 +0000

    add back accessors for the wizard to work

--- app/models/business.rb
+++ app/models/business.rb
@@ -4,6 +4,8 @@ class Business < ActiveRecord::Base
   attr_accessible :map, :id,:photo_url, :name, :url, :longitude, :latitude, :address1,:address2,:address3,:city, :province, :postal_code, :country, :phone_no, :text_excerpt, :distance, :avg_rating, :rating_img_url, :reviews
   
   attr_accessible :venue_type, :logo, :dna_excerpt, :dna_neighbourhood, :dna_atmosphere, :dna_pricepoint, :dna_category, :dna_dresscode, :dna_pictures, :dna_review, :dna_rating_conversation, :dna_rating_convenience, :dna_rating_comfort
+
+  attr_accessor :distance, :text_excerpt, :avg_rating, :rating_img_url, :reviews, :map
   
   def init
     @reviews = Array.new
