251381b8c0d676a58d3b8ec62bd0b9e784adb64b
Grace Li <fullofgrace88@gmail.com>
2011-03-13 20:52:00 -0400

    toggle cart add/remove buttons

--- db/schema.rb
+++ db/schema.rb
@@ -60,7 +60,6 @@ ActiveRecord::Schema.define(:version => 20110301042313) do
   end
 
   create_table "datecarts", :force => true do |t|
-    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
