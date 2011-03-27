6caeb130bbe15e54dea8a2a999191f3a228886c9
Date Ideas <dateideas@li248-56.members.linode.com>
2011-03-27 06:13:10 +0000

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110321003359) do
+ActiveRecord::Schema.define(:version => 20110321162346) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
