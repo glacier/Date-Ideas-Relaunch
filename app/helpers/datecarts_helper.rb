8f3531cdbe5104bc0743b0460f6c461578b08ee7
Grace Li <fullofgrace88@gmail.com>
2011-04-19 00:36:56 -0400

    generalized categories for datecart

--- app/helpers/datecarts_helper.rb
+++ app/helpers/datecarts_helper.rb
@@ -6,8 +6,9 @@ module DatecartsHelper
     category_display = Hash.new
     items.each do |item|
       # only display under the first venue_type that an item is classified under
-      list = item.business.venue_type.split(",")
-      venue = list[0]
+      # list = item.business.venue_type.split(",")
+      # venue = list[0]
+      venue = item.venue_type
       biz = item.business
       if category_display[venue].nil?
         category_display[venue] =  "<li>" <<  biz.name << " " << link_to('Remove', datecart_cart_item_path(datecart, item), :method => :delete, :remote => true) << "</li>"
