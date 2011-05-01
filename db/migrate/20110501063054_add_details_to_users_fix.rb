f594f3300476d3ca031054862818fba279053596
Date Ideas <dateideas@li248-56.members.linode.com>
2011-05-01 07:09:08 +0000

    migration patch

---
+++ db/migrate/20110501063054_add_details_to_users_fix.rb
@@ -0,0 +1,19 @@
+class AddDetailsToUsersFix < ActiveRecord::Migration
+  def self.up
+    add_column :users, :username, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :birthday, :date
+    add_column :users, :postal_code, :string
+    add_column :users, :gender, :string
+  end
+
+  def self.down
+    remove_column :users, :last_name
+    remove_column :users, :first_name
+    remove_column :users, :username
+    remove_column :users, :birthday, :date
+    remove_column :users, :postal_code, :string
+    remove_column :users, :gender, :string
+  end
+end
