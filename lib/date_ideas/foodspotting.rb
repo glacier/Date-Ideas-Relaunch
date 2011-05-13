c5aa3ec0d24300a252e97512698065a63de94c4b
Grace Li <fullofgrace88@gmail.com>
2011-05-12 22:59:10 -0400

    started foodspotting integration; cleaned up views some more

---
+++ lib/date_ideas/foodspotting.rb
@@ -0,0 +1,10 @@
+#Implements an adaptor for foodspotting api
+class DateIdeas::Foodspotting 
+  Consumer_Key = 'SKZpQcxoheuvj9Jnwp2w'
+  Consumer_Secret = 'YMZlgpUeDj8cRApyISDWWax0bC2xXnssXQQmGklH'
+  Request_Token_URL = "http://www.foodspotting.com/oauth/request_token"
+  Access_Token_URL = "http://www.foodspotting.com/oauth/access_token"
+  Authorize_URL = "http://www.foodspotting.com/oauth/authorize"
+  
+  
+end
