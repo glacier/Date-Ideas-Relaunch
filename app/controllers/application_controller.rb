9ff8cae48ad5f606089087efc8adc422accf7cfa
Grace Li <fullofgrace88@gmail.com>
2011-05-21 22:15:11 -0400

    started events

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :authenticate_user!
+  # before_filter :authenticate_user!
 
   private
     # gets the current cart in session or create a new one
