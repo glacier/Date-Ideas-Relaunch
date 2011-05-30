c33b084d91de96d63b454d7f91dd8af13401b0e9
Andy Pincombe <andy.pincombe@gmail.com>
2011-05-29 21:22:34 -0400

    Continuing capistrano integration [Continues #13567009]

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.3'
-gem 'devise', :git => 'git://github.com/plataformatec/devise.git'
+gem 'devise' #, :git => 'git://github.com/plataformatec/devise.git'
 gem 'omniauth'
 gem 'oa-openid', :require => 'omniauth/openid'
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,28 +1,34 @@
 set :application, "DateIdeas"
+set :scm, :git
 set :repository,  "git@github.com:will-lam/Date-Ideas-Relaunch.git"
 set :branch, "prod"
-set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-
 
 # Use single user installation settings
 set :rvm_type, :user
+set :rvm_ruby_string, "ruby-1.9.2-p136"
+
+set :deploy_to, "/mnt/apps/dateideas"
+set :rails_env, "production"
+
+default_run_options[:pty] = true
+set :use_sudo, false
+
+set :user, "dateideas"
+set :runner, "dateideas"
 
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "www1.getdateideas.com"                          # Your HTTP server, Apache/et
+role :app, "www1.getdateideas.com"                          # This may be the same as your `Web` server
+role :db,  "www1.getdateideas.com", :primary => true # This is where Rails migrations will run
+
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
