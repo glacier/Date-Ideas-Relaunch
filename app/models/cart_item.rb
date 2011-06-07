16ed494249e8b6ba4941baa20410949d1a91f337
Grace Li <fullofgrace88@gmail.com>
2011-06-07 18:01:20 -0400

    fixed print view to display events and businesses

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,4 +2,13 @@ class CartItem < ActiveRecord::Base
   belongs_to :business
   belongs_to :event
   belongs_to :datecart
+  def type
+    if business.nil? 
+      return 'event'
+    end
+    if event.nil?
+      return 'business'
+    end
+    return 'none'
+  end
 end
