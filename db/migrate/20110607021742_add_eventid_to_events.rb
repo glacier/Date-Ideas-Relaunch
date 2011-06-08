7013ccab690f9975168b25ae55b63a6d9b066ec0
Andy Pincombe <andy.pincombe@gmail.com>
2011-06-08 00:05:34 -0400

    AN: Removed redundant migration

--- db/migrate/20110607021742_add_eventid_to_events.rb
+++
@@ -1,9 +0,0 @@
-class AddEventidToEvents < ActiveRecord::Migration
-  def self.up
-    add_column :events, :eventid, :string
-  end
-
-  def self.down
-    remove_column :events, :eventid
-  end
-end
