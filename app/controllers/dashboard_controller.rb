929d32192c79b935855e286b4e29323761e8e602
Andy Pincombe <andy.pincombe@gmail.com>
2011-06-12 19:01:49 -0400

    AN: small refactor to sorting of datecarts

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,11 @@ class DashboardController < ApplicationController
   def index
     @user = current_user # This is ensured by authenticate_user! in the pre-block of app controller
     datecarts = @user.datecarts
-    datecarts.sort_by { |x| x.datetime }
+
+    # Define our sorting algorithm here
+    datecarts.sort_by do |cart|
+      cart.datetime
+    end
 
     @datecarts = []  #Sorts the array by their datetimes, then shows the most recent upcoming two
     @datecarts << datecarts.pop
@@ -15,7 +19,6 @@ class DashboardController < ApplicationController
     unless @profile
       # What do we do here if they haven't completed a profile?
       # Why is there a distinction between a profile and a user?
-      
     end
   end
 end
