2926f2670c2431233a4737bd946300ee0fffc9fe
Grace Li <fullofgrace88@gmail.com>
2011-06-15 01:27:23 -0400

    added fixtures and some functional tests for datecarts controller

--- test/functional/datecarts_controller_test.rb
+++ test/functional/datecarts_controller_test.rb
@@ -2,7 +2,14 @@ require 'test_helper'
 
 class DatecartsControllerTest < ActionController::TestCase
   setup do
-    @datecart = datecarts(:one)
+    @user = users(:demo)
+    @datecart = datecarts(:mycart)
+    
+    #assign demo to be an admin so that all tests have permission to run
+    @r1 = roles(:admin)
+    @r2 = roles(:user)
+    @a1 = assignments(:a1)
+    sign_in @user
   end
 
   test "should get index" do
@@ -20,10 +27,9 @@ class DatecartsControllerTest < ActionController::TestCase
     assert_difference('Datecart.count') do
       post :create, :datecart => @datecart.attributes
     end
-
     assert_redirected_to datecart_path(assigns(:datecart))
   end
-
+  
   test "should show datecart" do
     get :show, :id => @datecart.to_param
     assert_response :success
@@ -43,7 +49,7 @@ class DatecartsControllerTest < ActionController::TestCase
     assert_difference('Datecart.count', -1) do
       delete :destroy, :id => @datecart.to_param
     end
-
-    assert_redirected_to datecarts_path
   end
+  
+  # TODO write tests for the rest of the actions for the datecarts controller
 end
