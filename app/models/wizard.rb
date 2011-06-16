b587fbc566c80cd7173a16d5e24d476aa2c86f8f
Alvin Yap <alvin.yap@gmail.com>
2011-06-15 23:58:16 -0400

    Add district and subcategories to filters [Finishes #14517499,#14513031]

--- app/models/wizard.rb
+++ app/models/wizard.rb
@@ -11,15 +11,10 @@ class Wizard
   PRICE_POINTS = [['Budget','budget'],
                   ['Moderate','moderate'],
                   ['High Roller','high_roller']]
-  HEART_IMAGE_URL = { 1.0 => "/images/one_heart.gif", 
-                      2.0 => "/images/two_hearts.gif",
-                      3.0 => "/images/three_hearts.gif",
-                      4.0 => "/images/four_hearts.gif",
-                      4.5 => "/images/four_and_half_hearts.gif",
-                      5.0 => "/images/five_hearts.gif",
-                    }
 
-  attr_accessor :venue, :location, :price_point, :response, :businesses, :restaurant, :activity, :dessert, :neighbourhood_name, :events, :neighbourhoods, :neighbourhood
+  attr_accessor :venue, :location, :price_point, :response, :businesses, :restaurant,
+                :activity, :dessert, :neighbourhood_name, :events, :neighbourhoods, :neighbourhood,
+                :sub_categories, :sub_category
   
   def initialize(venue="", location="", price_point="")
     @venue = venue
