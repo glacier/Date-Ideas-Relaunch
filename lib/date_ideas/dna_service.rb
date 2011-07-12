3d163ab7343e746e64889212c731ca3a2d41b2ba
Andy Pincombe <&y@pincom.be>
2011-07-12 13:40:42 -0400

    AN: Fixed un-handled case where we don't have a postal code in the database

--- lib/date_ideas/dna_service.rb
+++ lib/date_ideas/dna_service.rb
@@ -1,93 +1,97 @@
 # coding: utf-8
 class DateIdeas::DnaService
 
-  CATEGORIES = { 'food' => ['food','restaurants'],
-                 'bars' => ['bars'],
-                 'activities_events' => ['active'],
-                 'nightlife' => ['nightlife'],
-                 'arts_entertainment' => ['arts'],
-               }
+  CATEGORIES = {'food' => ['food', 'restaurants'],
+                'bars' => ['bars'],
+                'activities_events' => ['active'],
+                'nightlife' => ['nightlife'],
+                'arts_entertainment' => ['arts'],
+  }
 
-  def search(venue_type,location,price_point,page = '1', per_page=10, neighbourhood = nil, sub_category = nil, city = nil, postal_code=nil, range=nil)
-    puts "search(#{venue_type},#{location},#{price_point},#{page}, #{per_page}, #{neighbourhood}, #{sub_category}, #{city}, #{postal_code}, #{range})"
+  def search(venue_type, location, price_point, page = '1', per_page=10, neighbourhood = nil, sub_category = nil, city = nil, postal_code=nil, range=nil)
+    Rails.logger.info "search(#{venue_type},#{location},#{price_point},#{page}, #{per_page}, #{neighbourhood}, #{sub_category}, #{city}, #{postal_code}, #{range})"
     neighbourhoods = []
     categories = []
     sql = ""
-    if ( sub_category.nil? || "all".eql?(sub_category))
+    if (sub_category.nil? || "all".eql?(sub_category))
       categories = CATEGORIES.fetch(venue_type)
     else
       categories.push(sub_category)
     end
-    if( 'Toronto'.eql?(city ))
-      if( neighbourhood.nil? || "all_neighbourhoods".eql?(neighbourhood) )
-        db_businesses = Business.search_by_district_subsection('Toronto',location,price_point,categories,page)
+    if ('Toronto'.eql?(city))
+      if (neighbourhood.nil? || "all_neighbourhoods".eql?(neighbourhood))
+        db_businesses = Business.search_by_district_subsection('Toronto', location, price_point, categories, page)
       else
-        db_businesses = Business.search_by_neighbourhood('Toronto',neighbourhood,price_point,categories,page)
+        db_businesses = Business.search_by_neighbourhood('Toronto', neighbourhood, price_point, categories, page)
       end
     else
-      db_businesses = Business.search_by_postal_code(city,postal_code,price_point,categories,page)
+      db_businesses = Business.search_by_postal_code(city, postal_code, price_point, categories, page)
     end
 
     db_businesses_no_exerpt = []
-    if(!db_businesses.nil?)
+    if (!db_businesses.nil?)
       db_businesses.each do |b|
-        if ( b.external_id.nil? )
-          if ( b.longitude.nil? || b.latitude.nil? )
+        if (b.external_id.nil?)
+          if (b.longitude.nil? || b.latitude.nil?)
             address = get_address(b)
             geocode = get_geocode(address)
             business = Business.find_by_id(b.id)
             business.longitude = geocode.lng
-            business.latitude  = geocode.lat
+            business.latitude = geocode.lat
             business.save
           end
-          b.neighbourhoods.collect {|hood| neighbourhoods.push(hood.neighbourhood) } #flatten list of neighbourhood objects and add it to the list.
-        elsif( b.text_excerpt.nil? || b.text_excerpt.size == 0 )
-          Rails.logger.info("business with no exerpt :" + b.external_id )
+          b.neighbourhoods.collect { |hood| neighbourhoods.push(hood.neighbourhood) } #flatten list of neighbourhood objects and add it to the list.
+        elsif (b.text_excerpt.nil? || b.text_excerpt.size == 0)
+          Rails.logger.info("business with no exerpt :" + b.external_id)
           db_businesses_no_exerpt.push(b)
         end
       end
     end
-    #grab from Yelp
+      #grab from Yelp
     yelp_adaptor = DateIdeas::YelpAdaptorV2.new(false)
 
-    if( 'Toronto'.eql?(city) && (postal_code.blank?) )
-      yelp_businesses = yelp_adaptor.search(city,CATEGORIES.fetch(venue_type), neighbourhoods)
+    if ('Toronto'.eql?(city) && (postal_code.blank?))
+      yelp_businesses = yelp_adaptor.search(city, CATEGORIES.fetch(venue_type), neighbourhoods)
     else
       neighbourhood = Neighbourhood.find_by_postal_code(postal_code)
-      yelp_businesses = yelp_adaptor.search_by_postal_code(CATEGORIES.fetch(venue_type), postal_code, city,
-                                                           neighbourhood.province, neighbourhood.country, range)
+      if neighbourhood
+        yelp_businesses = yelp_adaptor.search_by_postal_code(CATEGORIES.fetch(venue_type), postal_code, city,
+                                                             neighbourhood.province, neighbourhood.country, range)
+      else
+        yelp_businesses = []
+      end
     end
 
-    if( !db_businesses.nil? && db_businesses.size > 0 )
+    if (!db_businesses.nil? && db_businesses.size > 0)
       merged_businesses = merge(db_businesses, yelp_businesses)
     else
 
-      if( !yelp_businesses.nil? && yelp_businesses.size > 0 )
-        y_businesses =  []
-        #AY : figure out a better way of doing this.
+      if (!yelp_businesses.nil? && yelp_businesses.size > 0)
+        y_businesses = []
+          #AY : figure out a better way of doing this.
         yelp_businesses.each do |yb|
-          if( 'Montreal'.eql?(yb.city) || 'Montréal'.eql?(yb.city) || 'Toronto'.eql?(yb.city))
+          if ('Montreal'.eql?(yb.city) || 'Montréal'.eql?(yb.city) || 'Toronto'.eql?(yb.city))
             db_bs = Business.find_by_external_id(yb.external_id)
-            if( db_bs.nil? )
+            if (db_bs.nil?)
               Rails.logger.info("saving #{get_address(yb)} longitude : #{yb.longitude} latitude: #{yb.latitude}")
               begin
-              yb.save!
-              y_businesses.push(yb)
+                yb.save!
+                y_businesses.push(yb)
               rescue ActiveRecord::RecordInvalid
-                end
+              end
             else
               y_businesses.push(db_bs)
             end
           end
         end
-        merged_businesses = y_businesses.paginate( :page => page, :per_page => 8 )
+        merged_businesses = y_businesses.paginate(:page => page, :per_page => 8)
       else
         #no results from db and from yelp
-        merged_businesses = [].paginate(:page=>page,:per_page => 8 )
+        merged_businesses = [].paginate(:page=>page, :per_page => 8)
       end
     end
 
-    #search business details
+      #search business details
     merged_businesses.each do |b|
       if (!b.external_id.nil? && (b.dna_excerpt.nil? ||b.dna_excerpt.size == 0))
         business_detail = yelp_adaptor.business_detail(b.external_id)
