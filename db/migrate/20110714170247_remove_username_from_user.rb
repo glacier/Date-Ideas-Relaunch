a697e54c3297f66bab04bd70df6edbe7127283af
Grace Li <fullofgrace88@gmail.com>
2011-07-14 13:34:19 -0400

    GL - removed sig date add feature from being shown since its not yet implemented; moved and fixed some small presentational things in user, profile, dashboard; removed username column from user since its not even used

---
+++ db/migrate/20110714170247_remove_username_from_user.rb
@@ -0,0 +1,9 @@
+class RemoveUsernameFromUser < ActiveRecord::Migration
+  def self.up
+    remove_column :users, :username
+  end
+
+  def self.down
+    add_column :users, :username, :string
+  end
+end
