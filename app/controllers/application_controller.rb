fa1bbfc71c7aaec4e1e646ea691666c62cc43649
Grace Li <fullofgrace88@gmail.com>
2011-07-17 18:54:29 -0400

    protect site for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   # load_and_authorize_resource
-  # before_filter :authenticate_user!
+  before_filter :authenticate_user!
 
   private
     # gets the current cart in session or create a new one
